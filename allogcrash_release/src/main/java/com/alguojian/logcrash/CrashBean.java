7535eed48cff3161133657c0bb5699a044e1a669
alguojian <jzhszy@foxmail.com>
2020-06-10 11:14:44 +0800

    ..

---
+++ allogcrash_release/src/main/java/com/alguojian/logcrash/CrashBean.java
@@ -0,0 +1,14 @@
+package com.alguojian.logcrash;
+
+import org.litepal.crud.LitePalSupport;
+
+import java.io.Serializable;
+
+/**
+ * ${Descript}
+ *
+ * @author alguojian
+ * @date 2018/8/15
+ */
+public class CrashBean extends LitePalSupport implements Serializable {
+}
---
+++ allogcrash_release/src/main/java/com/alguojian/logcrash/OtherNewsBean.java
@@ -0,0 +1,14 @@
+package com.alguojian.logcrash;
+
+import org.litepal.crud.LitePalSupport;
+
+import java.io.Serializable;
+
+/**
+ * ${Descript}
+ *
+ * @author alguojian
+ * @date 2018/8/15
+ */
+public class OtherNewsBean extends LitePalSupport implements Serializable {
+}
